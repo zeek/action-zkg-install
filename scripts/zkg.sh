84b5275a816e130a4d90b82b579af24e20abc6ec
Christian Kreibich <christian@corelight.com>
2021-04-28 21:47:12 -0700

    Optionally add Zeek parser run after successful zkg installation
    This adds -l|--load-packages to the Docker entrypoint. It runs zkg's
    packages.zeek script through zeek -a when the zkg package installation
    succeeded. This can catch basic problems in a package that doesn't
    include tests, but also detect more subtle ones that its tests might
    not cover. The corresponding action input argument is `load_packages`.
    
    This also moves the artifact logic around a bit, leaving it up to each
    test script how to preserve artifacts.

--- scripts/zkg.sh
+++ scripts/zkg.sh
@@ -11,13 +11,18 @@ fi
 
 echo "Installing package from '$pkgurl', ${pkgverdesc:-unversioned}"
 zkg install --force $pkgverarg $pkgurl || {
-    for log in $(zeek-config --prefix)/var/lib/zkg/logs/*-build.log; do
+    zkg_logs_dir=$(zeek-config --prefix)/var/lib/zkg/logs
+    for log in $zkg_logs_dir/*-build.log; do
         if [ -r "$log" ]; then
             echo "*** $log:"
             cat $log
             echo
         fi
     done
+
+    if [ -d "$zkg_logs_dir" ] && [ -d "$ARTIFACTS_DIR" ]; then
+        cp -pr $zkg_logs_dir $ARTIFACTS_DIR
+    fi
 
     exit 1
 }
